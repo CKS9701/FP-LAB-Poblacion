67c94d74c70d073336dc3c18c340b064742a3be3
CKS9701 <ansjimzam@alum.us.es>
2025-11-03 09:38:17 +0100

    Casi terminado

--- src/poblacion.py
+++ src/poblacion.py
@@ -3,6 +3,8 @@ import csv
 from collections import namedtuple
 RegistroPoblacion = namedtuple('RegistroPoblacion', 'pais, codigo, año, censo')
 
+from matplotlib import pyplot as plt
+
 def lee_poblaciones(fichero: str) -> list[RegistroPoblacion]:
     with open(fichero, encoding='utf-8') as f:
         lector = csv.reader(f)
@@ -22,3 +24,31 @@ def calcula_paises(poblaciones: list[RegistroPoblacion]) -> list[str]:
 
     return sorted(paises)
 
+def filtra_por_pais(poblaciones:list[RegistroPoblacion], nombre_o_codigo: str) -> list[tuple]:
+    ret = []
+    for x in poblaciones:
+        if nombre_o_codigo == x.pais or nombre_o_codigo == x.codigo:
+            ret.append((x.año, x.censo))
+
+    return ret
+
+def filtra_por_paises_y_anyo(poblaciones: list[RegistroPoblacion], anyo: int, paises: set[str]) -> list[tuple]:
+    ret = []
+    for x in poblaciones:
+        if x.pais in paises and x.año == anyo:
+            ret.append((x.pais, x.censo))
+
+    return ret
+
+def muestra_evolucion_poblacion(poblaciones: list[RegistroPoblacion], nombre_o_codigo: str) -> None:
+    lista_anyos = []
+    lista_habitantes = []
+    for x in poblaciones:
+        if x.pais == nombre_o_codigo or x.codigo == nombre_o_codigo:
+            lista_anyos.append(x.año)
+            lista_habitantes.append(x.censo)
+            nombre_pais = x.pais
+
+    plt.title(f"Evolucion de la poblacion de {nombre_pais}")
+    plt.plot(lista_anyos, lista_habitantes)
+    plt.show()
--- src/poblacion_test.py
+++ src/poblacion_test.py
@@ -16,8 +16,31 @@ def test_calcula_paises(poblaciones: list[RegistroPoblacion]) -> None:
     print(f"Los 10 primeros paises son {paises[:10]}")
     print(f"Los 10 últimos paises son {paises[-10:]}")
 
+def test_filtra_por_pais(poblaciones:list[RegistroPoblacion], nombre_o_codigo: str) -> None:
+    print("Probando filtra_por_pais()...")
+
+    datos = filtra_por_pais(poblaciones, nombre_o_codigo)
+    print(f"El país tiene {len(datos)} datos")
+    print(f"Los 5 primeros datos son {datos[:5]}")
+    print(f"Los 5 últimos datos son {datos[-5:]}")
+    
+def test_filtra_por_paises_y_anyo(poblaciones: list[RegistroPoblacion], anyo: int, paises: set[str]) -> None:
+    print("Probando filtra_por_paises_y_anyo()...")
+
+    datos = filtra_por_paises_y_anyo(poblaciones, anyo, paises)
+    print(f"Se han encontrado {len(datos)} datos")
+    print(f"Los 5 primeros son {datos[:5]}")
+    print(f"Los 5 últimos son {datos[-5:]}")
+
+def test_muestra_evolucion_poblacion(poblaciones: list[RegistroPoblacion], nombre_o_codigo: str) -> None:
+    print("Probando muestra_evolucion_poblacion()...")
+    muestra_evolucion_poblacion(poblaciones, nombre_o_codigo)
+
 if __name__ == '__main__':
     # poblaciones = test_lee_poblaciones('data/population.csv')
     poblaciones = lee_poblaciones('data/population.csv')
 
     # test_calcula_paises(poblaciones)
+    # test_filtra_por_pais(poblaciones, "EUU")
+    # test_filtra_por_paises_y_anyo(poblaciones, 1970, {"Latin America & Caribbean", "European Union", "East Asia & Pacific (IDA & IBRD countries)"})
+    test_muestra_evolucion_poblacion(poblaciones, "EUU")
